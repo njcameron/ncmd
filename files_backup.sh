933246c0f5a2132332ecc2c1241ea0201718c33a
Neil Cameron <neil.cameron@cameronandwilding.com>
2015-06-28 15:31:54 +0100

    add license

--- files_backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PATH_TO_BACKUP=/Users/neil/Sites/ferko/sites/default
-BACKUP_FILE=/Users/neil/Sites/ferko/backups/files/ferko_files_backup.`date +"%Y-%m-%d-%H%M"`.tar.gz
-
-/usr/bin/tar -cpzf $BACKUP_FILE $PATH_TO_BACKUP
-
-echo "$BACKUP_FILE was created ON CRON" > /var/log/cron.log
